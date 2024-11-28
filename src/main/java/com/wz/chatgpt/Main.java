a36716fdeeaa59736c5ce6a70e603e5e7194c269
GGBond <2792289199@qq.com>
2024-11-28 15:29:59 +0800

    项目初始空工程代码

---
+++ src/main/java/com/wz/chatgpt/Main.java
@@ -0,0 +1,11 @@
+package com.wz.chatgpt;
+
+/**
+ * @auther Mr.wang
+ * @date 2024/11/28 - 15:20
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
